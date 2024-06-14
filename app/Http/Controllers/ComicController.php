80ae12ffb23696c92b5528c579c01945ddca049e
lucasens1 <lucasensini1@gmail.com>
2024-06-14 18:45:20 +0200

    completed import, usable table, to see details of a single comic

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Prelevo tutti i comics
+        $comicsList = Comic::all();
+        // Restituisco la page index, allego array
+        return view('comics.index', compact('comicsList'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comic $comic)
+    {
+        // Restituisco la view in dettaglio del fumetto, passando l'intero oggetto
+        return view('comics.show', compact('comic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    
+<div class="container">
+    <h1>
+        Lista dei fumetti :
+    </h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comicsList as $item)
+                <tr>
+                    <th scope="row">{{ $item->id }}</th>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->type }}</td>
+                    <td><button class="btn btn-success"><a href="{{route('comics.show',['comic'=>$item->id])}}">Dettagli</a></button></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
