d8318684c82af8a1df58dc230851cd74f49fa244
lucasens1 <lucasensini1@gmail.com>
2024-06-17 16:12:04 +0200

    basic modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,18 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
+
+// Prendo i bottoni cancella
+const deleteButtons = document.querySelectorAll('.btn-danger');
+
+console.log(deleteButtons);
+
+// Per tutti i bottoni presi, aggiungo l'Event Listener
+deleteButtons.forEach(curBtn => {
+    curBtn.addEventListener('click', e => {
+        const decision = confirm('Vuoi cancellare?');
+        if(!decision) {
+            e.preventDefault();
+        }
+    });
+});
