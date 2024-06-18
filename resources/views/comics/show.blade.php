9c732bb9e021ee48e7175a5c411a684f2c16e160
lucasens1 <lucasensini1@gmail.com>
2024-06-18 18:59:01 +0200

    minor fix

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1 class="text-center my-5">Dettagli Fumetto : {{$comic->title}}</h1>
-        <div class="d-flex align-items-center">
+        <div class="d-flex align-items-start">
             <img class="w-25 mt-2 mb-3"   src="{{($comic->thumb)}}" alt="Immagine non disponibile">
             <div class="mx-2">
                 <h6>{{ $comic->description}}</h6>
